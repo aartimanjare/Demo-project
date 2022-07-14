3a8f8bb27c00ecfa585f26e0a82cf2adde8336c1
aartimanjare <108117947+aartimanjare@users.noreply.github.com>
2022-07-14 11:53:09 +0530

    Update program-1.py

--- program-1.py
+++ program-1.py
@@ -1,23 +1,27 @@
 '''Python program to convert number into sring
 that contains raindrop sound'''
 
-def convert(number):
-    result = " "
-    
-# if has 3 as a factor, "pling" to result    
-    if number % 3 == 0:
-       result += "Pling"
-    
-# if has 5 as a factor, "plang" to result    
-    if number % 5 == 0:
-       result += "Plang"
+def convert():
+    num = int(input('enter a number: '))
 
-# if has 7 as a factor, "plong" to result    
-    if number % 7 == 0:
-       result += "Plong"
-    
-# if doesn't have factor, 	   
-    if result is " ":
-		return str(number)
+    if num%5 == 0 and num%3 == 0:
+        print("pling plang")
 
-return result
+    elif num%5 == 0 and num%7 == 0:
+        print('Plang plong')
+
+    elif num%3 == 0 and num%7 == 0:
+        print('pling plong')
+
+    elif num%3 == 0:
+        print('pling')
+
+    elif num%5 == 0:
+        print('Plang')
+
+    elif num%7 == 0:
+        print('Plong')
+
+    else:
+        print(num)
+convert()
